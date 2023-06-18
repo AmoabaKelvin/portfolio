fa7ac3badc049164547eb0234d17b67f4353e852
Kelvin Amoaba <97001695+AmoabaKelvin@users.noreply.github.com>
2023-06-18 00:10:45 +0000

    remove hardcoded cards and map project cards

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,17 +1,15 @@
+'use client';
 import ProjectCard from '@/components/ProjectCard';
-import React from 'react';
+import { portolioProjects } from '@/data';
 
 const ProjectsPage = () => {
   return (
-    <div className="mt-8 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-2">
+    <div className="px-4 mx-auto mt-8 mb-2 max-w-7xl sm:px-6 lg:px-8">
       {/* Projects card, 3 cards per row */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        {portolioProjects.map((project) => (
+          <ProjectCard {...project} key={project.id} />
+        ))}
       </div>
     </div>
   );
