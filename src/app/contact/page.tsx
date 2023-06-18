6a8e9f3f44f974b4fa95d8aa2190782878ddf154
Kelvin Amoaba <97001695+AmoabaKelvin@users.noreply.github.com>
2023-06-18 15:02:50 +0000

    add contact page form and emailjs to send email

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,100 @@
+'use client';
+
+import emailjs from '@emailjs/browser';
 import React from 'react';
 
 const ContactsPage = () => {
-  return <div>ContactsPage</div>;
+  const form = React.useRef();
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID as string,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID as string,
+        form.current as any,
+        process.env.NEXT_PUBLIC_PUBLICKEY as string
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <React.Fragment>
+      <div className="px-4 mx-auto mt-8 mb-2 max-w-7xl sm:px-6 lg:px-8">
+        <div className="max-w-screen-md px-4 py-8 mx-auto lg:py-16">
+          <h2 className="mb-4 text-4xl font-extrabold tracking-tight text-center text-gray-900 dark:text-white">
+            Contact Me
+          </h2>
+          <p className="mb-8 font-light text-center text-gray-500 lg:mb-16 dark:text-gray-400 sm:text-xl">
+            Reach out to me if you have any questions or just want to say hi! I
+            will get back to you as soon as I can.
+          </p>
+          <form className="space-y-8" ref={form as any} onSubmit={sendEmail}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="wonderfulname@chosenagent.com"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="subject"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Subject
+              </label>
+              <input
+                type="text"
+                id="subject"
+                name="subject"
+                className="block w-full p-3 text-sm text-gray-900 border border-gray-300 rounded-lg shadow-sm bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="Just a hint for me to know what you want to talk about"
+                required
+              />
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+              >
+                Your message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                rows={6}
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Your wonderful message to me"
+              />
+            </div>
+            <button
+              type="submit"
+              className="px-5 py-3 text-sm font-medium text-center text-black rounded-lg bg-primary-700 sm:w-fit bg-[#f3c158]"
+            >
+              Send message
+            </button>
+          </form>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default ContactsPage;
