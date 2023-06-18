2959fa0f84a88ba8c94eeabb4e5479b3bd323236
Kelvin Amoaba <97001695+AmoabaKelvin@users.noreply.github.com>
2023-06-18 22:58:52 +0000

    increase bottom margin

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,7 +4,7 @@ import { portolioProjects } from '@/data';
 
 const ProjectsPage = () => {
   return (
-    <div className="px-4 mx-auto mt-8 mb-2 max-w-7xl sm:px-6 lg:px-8">
+    <div className="px-4 mx-auto mt-8 mb-14 max-w-7xl sm:px-6 lg:px-8">
       {/* Projects card, 3 cards per row */}
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
         {portolioProjects.map((project) => (
