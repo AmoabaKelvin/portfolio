072a123212827d20eaf39fc3502cfecf13a7403b
AmoabaKelvin <kel.amoaba@gmail.com>
2023-12-24 21:51:09 +0000

    Add projects array to projects.ts

---
+++ src/projects.ts
@@ -0,0 +1,49 @@
+export const projects = [
+  {
+    name: 'iShortn',
+    link: 'https://ishortn.ink',
+    description:
+      'A dynamic links and url shortener and QR code generator. Built as a result of firebase shutting down their dynamic links service and other alternatives being too expensive. Now have 30+ users and over 200 daily uses.',
+  },
+  {
+    name: 'Bazaar',
+    link: 'https://app.vela.partners',
+    description:
+      'Bazaar is an automated web scraping tool, capable of developing web scrapers for any website in a few clicks, including deployment to the cloud.',
+  },
+  {
+    name: 'OpenStatus',
+    link: 'https://openstatus.dev',
+    description:
+      'Active contributor to OpenStatus. 🏓 The open-source monitoring platform with incident management 🏓',
+  },
+  {
+    name: 'Scrapy BigQuery Pipeline',
+    link: 'https://github.com/8W9aG/scrapy-bigquery',
+    description:
+      'Contributed to the development of an open-source bigquery pipeline for Scrapy framework',
+  },
+  {
+    name: 'Vinyl',
+    link: 'https://github.com/AmoabaKelvin/vinyl-backend',
+    description: `A music store built with Django Rest Framework, PostgreSQL and integrated with Stripe
+    This project is a backend API for a music store, with features like authentication, authorization, payment processing.
+    It allows users to buy music and artists to sell their music by uploading it to the platform and getting paid for every sale.
+    `,
+  },
+  {
+    name: 'Studyfuse',
+    link: '',
+    description:
+      'Studyfuse is an online forum for students and teachers to ask questions and share ideas, developed during my time in college for fellow students.',
+  },
+  {
+    name: 'Kairos',
+    description: `
+      A laboratory management system built with Python, Django, PostgreSQL and Bootstrap.
+      Kairos solved the problem of record keeping and management of patients' data as well as financial records.
+      It allowed to register new parents, capture their data, and store them in a database. Performing revenue analysis and generating reports.
+      `,
+    link: 'https://github.com/AmoabaKelvin/kairos-medical-center',
+  },
+];
