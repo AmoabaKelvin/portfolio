5bc0e59406180f3041494c170d1dadb45a960438
AmoabaKelvin <kel.amoaba@gmail.com>
2023-12-25 21:39:42 +0000

    Add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import { MetadataRoute } from 'next';
+
+// fetch all posts from the file system and return as array
+function getPosts(): string[] {
+  const files = fs.readdirSync(process.cwd() + '/src/posts');
+  // get only md files and remove .md from file name
+  return files.filter((fn) => fn.endsWith('.md')).map((fn) => fn.slice(0, -3));
+}
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const posts = getPosts();
+
+  return [
+    {
+      url: 'https://kelvinamoaba.live',
+      lastModified: new Date().toISOString(),
+    },
+    ...posts.map((slug) => ({
+      url: `https://kelvinamoaba.live/blog/${slug}`,
+      lastModified: new Date().toISOString(),
+    })),
+  ];
+}
