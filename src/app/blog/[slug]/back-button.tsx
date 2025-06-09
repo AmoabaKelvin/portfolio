1bc42102057190d01022f0c6de5252113c05aff0
AmoabaKelvin <kel.amoaba@gmail.com>
2025-06-09 20:45:15 +0000

    fix: enhance back button functionality to navigate home if no history exists

--- src/app/blog/[slug]/back-button.tsx
+++ src/app/blog/[slug]/back-button.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { FaArrowLeft } from 'react-icons/fa';
+import { useRouter } from "next/navigation";
+import { FaArrowLeft } from "react-icons/fa";
 
 const BackButton = ({ className }: { className?: string }) => {
   const router = useRouter();
@@ -9,7 +9,13 @@ const BackButton = ({ className }: { className?: string }) => {
   return (
     <span
       className={`flex items-center ${className}`}
-      onClick={() => router.back()}
+      onClick={() => {
+        if (window.history.length > 1) {
+          router.back();
+        } else {
+          router.push('/');
+        }
+      }}
     >
       <FaArrowLeft />
       <span className="ml-2 text-black hover:underline underline-offset-4 hover:cursor-pointer">
